68cef7c8d60ae629f47a2a1524a7954a6df4a1c6
besfortcollaku-cpu <besfort.collaku@gmail.com>
2026-01-12 18:57:18 +0100

    add header

--- src/index.ts
+++ src/index.ts
@@ -42,6 +42,23 @@ app.use(cors({
 }));
 app.options("*", cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-admin-secret"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 /* ---------------- HEALTH ---------------- */
 app.get("/health", (_req, res) => res.send("ok"));
