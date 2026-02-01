1d06e1d2a10a288f6148fc75d3e4bab434c9dcee
cottyPhone <besfort.collaku@gmail.com>
2026-02-01 14:49:41 +0100

    ad 50 alert

--- src/index.ts
+++ src/index.ts
@@ -170,6 +170,7 @@ app.post("/api/pi/verify", async (req, res) => {
 
 /* ---------------- REWARDS ---------------- */
 app.post("/api/rewards/ad-50", async (req,res)=>{
+console.log("AD +50 HIT", req.user.uid);
   try{
     const { uid } = await requirePiUser(req);
     const nonce = String(req.body?.nonce||"");
